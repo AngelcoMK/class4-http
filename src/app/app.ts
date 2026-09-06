d29b47b0275f599b7e6a5bbe25a13d2056778332
Angelco <ampevangelco3@gmail.com>
2026-09-06 22:35:59 +0200

    Complete HTTP GET example 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal, type OnInit } from '@angular/core';
+import type { Post } from './post.type';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('class4-http');
+export class App implements OnInit {
+  private postService = inject(PostService);
+
+  posts = signal<Post[]>([]);
+  isLoading = signal(false);
+
+  ngOnInit() {
+    this.isLoading.set(true);
+
+    this.postService.getPosts().subscribe((posts) => {
+      this.posts.set(posts);
+      this.isLoading.set(false);
+      console.log('Posts inside of the http call', this.posts, Date.now());
+    });
+
+    console.log('Posts outside of the http call', this.posts, Date.now());
+  }
 }

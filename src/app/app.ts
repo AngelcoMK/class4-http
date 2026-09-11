89a7526022585fee0aa8855761f2b7a2d1e78d9a
Angelco <ampevangelco3@gmail.com>
2026-09-11 20:23:25 +0200

    Complete Class 6 Angular Signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,8 @@
-import { Component, inject, signal, type OnInit } from '@angular/core';
-import type { Post } from './post.type';
-import { PostService } from './post.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private postService = inject(PostService);
-
-  posts = signal<Post[]>([]);
-  isLoading = signal(false);
-
-  ngOnInit() {
-    this.isLoading.set(true);
-
-    this.postService.getPosts().subscribe((posts) => {
-      this.posts.set(posts);
-      this.isLoading.set(false);
-      console.log('Posts inside of the http call', this.posts, Date.now());
-    });
-
-    console.log('Posts outside of the http call', this.posts, Date.now());
-  }
-}
+export class App {}
